816a6854d14dd988d6dacfc02171ed64b6bf781e
Abdul Wasay <wasay4681@gmail.com>
2025-02-01 05:29:46 +0500

    initializing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>DB Project</h1>
+      <p align="left" className="Body">Group members: 
+        <ul align="left">
+          <li>Abdul Wasay</li>
+          <li>Shawaiz Ali</li>
+          <li>Hassan Raza</li>
+        </ul>
+      </p>
     </div>
   );
 }
