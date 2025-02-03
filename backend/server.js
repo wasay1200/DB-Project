68168c4ef01d1179b8937b48e176654be5bf9026
unknown <shawaizali788@gmail.com>
2025-02-03 16:19:54 +0600

    shawaiz's first commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.get('/', (req, res) => {
-    res.send(`Hello From Node.js Backend!!`)
+    res.send(`Hello guyys!!`)
 });
 app.listen(PORT, () => {
     console.log(`Server is runnning on http://localhost:${PORT}`)
