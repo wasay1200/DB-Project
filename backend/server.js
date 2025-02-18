8243ff84a14c6ba4e54304e946ed43380b2fa18a
Has090 <mhassan090110@gmail.com>
2025-02-18 11:31:41 +0500

    Hassan's first commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.get('/', (req, res) => {
-    res.send(`Hello guyys!!`)
+    res.send(`Hello guyys!! This is edited by hassan`)
 });
 app.listen(PORT, () => {
     console.log(`Server is runnning on http://localhost:${PORT}`)
