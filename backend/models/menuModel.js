899aba10cfda0da9a534d44ff7c77aeebecbc702
Has090 <mhassan090110@gmail.com>
2025-04-15 12:51:33 +0500

    add some functions to menu model

--- backend/models/menuModel.js
+++ backend/models/menuModel.js
@@ -5,6 +5,26 @@ const Menu = {
         const pool = await poolPromise;
         const result = await pool.request().query('SELECT * FROM Menu');
         return result.recordset;
+    },
+    async GetMenuItemsWithRatings() {
+        try
+        {
+           const pool = await poolPromise;
+           const result = await pool.request().query(`SELECT 
+           m.*,
+           COALESCE(AVG(dr.rating), 0) AS average_rating,
+           COUNT(dr.review_id) AS review_count
+           FROM Menu m
+           LEFT JOIN Dish_Reviews dr ON m.menu_id = dr.menu_id
+           GROUP BY m.menu_id, m.name
+           ORDER BY m.category, m.name;`);
+        }
+        catch (err) {
+            console.error('SQL error', err);
+            return err;
+        }  
+        return result.recordset;
     }
+    
 };
 module.exports = Menu;
