a62b719698f46a5e9b3842aab1db60879d299ed0
unknown <shawaizali788@gmail.com>
2025-04-15 21:09:21 +0600

    check menu endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const {sql, poolPromise} = require('./config/db.js');
+const {sql, poolPromise} = require('./db.js');
 const PORT = 5000;
 app.use(cors());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+
+const menuRoute = require('./routes/menuRoute'); // adjust path as needed
+app.use('/api/menu', someRoute);
+
 
 app.get('/', (req, res) => {
     res.send(`Use path /api/{tablename} for query results for that table!`);
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
