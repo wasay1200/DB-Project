7b63b71b81ebff1cd96aa4f4a04b9c4c1fabcd86
Abdul Wasay <wasay4681@gmail.com>
2025-04-16 16:09:51 +0500

    fixed GetMenuItemsWithRatings Query

--- backend/models/menuModel.js
+++ backend/models/menuModel.js
@@ -11,19 +11,20 @@ const Menu = {
         {
            const pool = await poolPromise;
            const result = await pool.request().query(`SELECT 
-           m.*,
+            m.name, m.price,
            COALESCE(AVG(dr.rating), 0) AS average_rating,
            COUNT(dr.review_id) AS review_count
            FROM Menu m
            LEFT JOIN Dish_Reviews dr ON m.menu_id = dr.menu_id
-           GROUP BY m.menu_id, m.name
-           ORDER BY m.category, m.name;`);
+           GROUP BY m.name,m.price
+           ORDER BY m.name;`);
+           return result.recordset;
         }
         catch (err) {
             console.error('SQL error', err);
             return err;
         }  
-        return result.recordset;
+        
     }
     
 };
