efad09fdedf43e7c2c7ec4a51ac3220b38783864
Has090 <mhassan090110@gmail.com>
2025-04-18 15:30:31 +0500

    staffratings completed

--- backend/models/staffratingsModel.js
+++ backend/models/staffratingsModel.js
@@ -10,7 +10,50 @@ const StaffRatings = {
             console.error('SQL error', err);
             return err;
         }
-    }
+    },
+
+    async GetAllStaff() {
+        try {
+            const pool = await poolPromise;
+            const result = await pool.request().query(`SELECT * FROM Staff`);
+            return result.recordset;
+        } catch (err) {
+            console.error('SQL error', err);
+            return err;
+        }
+    },
+    
+    async GetStaffRating(staff_id) {
+        try {
+            const pool = await poolPromise;
+            const result = await pool.request().input('staff_id', sql.Int,staff_id ).query(`SELECT sr.*, u.name AS user_name
+         FROM Staff_Ratings sr
+         JOIN Users u ON sr.user_id = u.user_id
+         WHERE sr.staff_id = @staff_id
+         ORDER BY sr.rating_id DESC`);
+            return result.recordset;
+        } catch (err) {
+            console.error('SQL error', err);
+            return err;
+        }
+   
+    } ,
+
+    async CreateStaffRatings(staff_id ,user_id ,rating ,feedback ) {
+        try {
+            const pool = await poolPromise;
+            const result = await pool.request().Input('staff_id', sql.Int,staff_id )
+            Input('user_id', sql.Int,user_id ).Input('rating', sql.Int,rating).Input('feedback', sql.NVarChar,feedback ).
+            query(`INSERT INTO Staff_Ratings (staff_id, user_id, rating, feedback)
+            VALUES (@staff_id, @user_id, @rating, @feedback)`);
+            return result.recordset;
+        } catch (err) {
+            console.error('SQL error', err);
+            return err;
+        }
+    } 
+
+
 };
 
 module.exports = StaffRatings;
