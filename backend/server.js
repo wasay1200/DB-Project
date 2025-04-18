7dc5cd89ff230ed338654fa2e29f28304c229e16
unknown <shawaizali788@gmail.com>
2025-04-18 13:01:52 +0600

    add routes

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const {sql, poolPromise} = require('./config/db.js');
+const {sql, poolPromise} = require('./db.js');
 const PORT = 5000;
 app.use(cors());
 //app.use(bodyParser.json());
 
 const menuRoute = require('./routes/menuRoute'); // adjust path as needed
+const reservationsRoute = require('./routes/reservationRoute');
+const ordersRoute = require('./routes/orderRoute');
+const dishReviewsRoute = require('./routes/dishreviewsRoute');
+const staffRatingsRoute = require('./routes/staffratingsRoute');
+
+
 app.use('/api/menu', menuRoute);
+app.use('/api/reservations', reservationsRoute);
+app.use('/api/orders', ordersRoute);
+app.use('/api/dish-reviews', dishReviewsRoute);
+app.use('/api/staff-ratings', staffRatingsRoute);
 
 
 app.get('/', (req, res) => {
